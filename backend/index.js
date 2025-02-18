43e8bcfc8647608bb739244592398b9cfffcb794
Dhruv <dhruvgoyal014@gmail.com>
2025-02-18 21:55:53 +0530

    project created

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
